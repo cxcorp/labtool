10e52a12ae5699ead6a1d426736673ce81337a29
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-12-22 00:36:04 +0200

    Split FeedbackForm to components

--- labtool2.0/src/components/pages/review-student/FeedbackForm.js
+++ labtool2.0/src/components/pages/review-student/FeedbackForm.js
@@ -3,6 +3,64 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { Button, Form, Input } from 'semantic-ui-react'
 
+const PointsField = ({ initialPoints, maxPoints, forwardInputRef }) => (
+  <Form.Field>
+    <label>Points 0-{maxPoints}</label>
+    <Input
+      ref={forwardInputRef}
+      name="points"
+      defaultValue={initialPoints}
+      type="number"
+      step="0.01"
+      style={{ width: '150px', align: 'center' }}
+    />
+  </Form.Field>
+)
+
+PointsField.propTypes = {
+  forwardInputRef: PropTypes.object,
+  initialPoints: PropTypes.number,
+  maxPoints: PropTypes.number
+}
+
+const FeedbackField = ({ forwardRef, initialText }) => (
+  <div ref={forwardRef}>
+    {/* Do not change the contents of this div.
+    * ReviewStudent::copyChecklistOutput relies on its current structure
+    * to copy the checks' generated text to the TextArea.
+    * TextArea cannot be ref'd directly because it is a stateless
+    * component.
+    */}
+    <Form.TextArea
+      defaultValue={initialText}
+      name="comment"
+      style={{ width: '500px', height: '250px' }}
+    />
+  </div>
+)
+
+FeedbackField.propTypes = {
+  forwardRef: PropTypes.object,
+  initialText: PropTypes.string
+}
+
+const ButtonControlsField = ({ cancelLinkHref }) => (
+  <Form.Field>
+    <Button className="ui center floated green button" type="submit">
+      Save
+    </Button>
+    <Link to={cancelLinkHref} type="Cancel">
+      <Button className="ui center floated button" type="cancel">
+        Cancel
+      </Button>
+    </Link>
+  </Form.Field>
+)
+
+ButtonControlsField.propTypes = {
+  cancelLinkHref: PropTypes.string
+}
+
 const FeedbackForm = ({
   cancelLinkHref,
   forwardReviewPointsRef,
@@ -15,44 +73,19 @@ const FeedbackForm = ({
   return (
     <Form onSubmit={onSubmit}>
       <Form.Group inline unstackable>
-        <Form.Field>
-          <label>Points 0-{weekMaxPoints}</label>
-          <Input
-            ref={forwardReviewPointsRef}
-            name="points"
-            defaultValue={initialReviewPoints}
-            type="number"
-            step="0.01"
-            style={{ width: '150px', align: 'center' }}
-          />
-        </Form.Field>
+        <PointsField
+          forwardInputRef={forwardReviewPointsRef}
+          initialPoints={initialReviewPoints}
+          maxPoints={weekMaxPoints}
+        />
       </Form.Group>
-      <label>Feedback</label>
       <Form.Group inline unstackable>
-        <div ref={forwardReviewTextRef}>
-          {/* Do not change the contents of this div.
-            * ReviewStudent::copyChecklistOutput relies on its current structure
-            * to copy the checks' generated text to the TextArea.
-            * TextArea cannot be ref'd directly because it is a stateless
-            * component.
-            */}
-          <Form.TextArea
-            defaultValue={initialReviewText}
-            name="comment"
-            style={{ width: '500px', height: '250px' }}
-          />
-        </div>
+        <FeedbackField
+          forwardRef={forwardReviewTextRef}
+          initialText={initialReviewText}
+        />
       </Form.Group>
-      <Form.Field>
-        <Button className="ui center floated green button" type="submit">
-          Save
-        </Button>
-        <Link to={cancelLinkHref} type="Cancel">
-          <Button className="ui center floated button" type="cancel">
-            Cancel
-          </Button>
-        </Link>
-      </Form.Field>
+      <ButtonControlsField cancelLinkHref={cancelLinkHref} />
     </Form>
   )
 }
